1c646541bbefdc7bf80fda0745db895b295f3782
Bizz247 <dbacon247@live.com>
2019-10-23 13:20:04 -0700

    added preloaded inventory method to load default inventory at start

--- OnlineStore2/InventoryItem.cs
+++ OnlineStore2/InventoryItem.cs
@@ -14,20 +14,38 @@ namespace Online_Store
         public string ProductName { get; set; }
         public int InventoryQuanity { get; set; }
         public string Seller { get; set; }
-        public decimal Price { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedDate { get; private set; }
 
-        public static void addInvetoryItem()
+        public static void addInvetoryItem(int productNumber,string productName,int quanity, string seller, double price)
         {
-
+            InventoryItem item = new InventoryItem();
+            item.CreatedDate = InventoryItem.generateDateCreated();
+            item.InventoryQuanity = quanity;
+            item.Price = price;
+            item.ProductName = productName;
+            item.ProductNumber = productNumber;
+            item.Seller = seller;
+            inventoryDatabase.Add(item);
         }
         public static void delInventoryItem()
         {
 
         }
+        public static DateTime generateDateCreated()
+        {
+            DateTime date = DateTime.Now;
+            return date;
+        }
         public static IEnumerable<InventoryItem>searchProductByName(string productname)
         {
             return inventoryDatabase.Where(a => a.ProductName.Contains(productname));
+        }
+        public static void preloadInventoryOnStart()
+        { 
+            addInvetoryItem(1, "Red Apple", 20, "Apple an Apple Inc.", 0.50);
+            addInvetoryItem(2, "Oranges", 5, "Californiaproduce.com", 1.00);
+            addInvetoryItem(3, "0day RCE against azure", 1, "wouldn't you like to know", 1000000.00);
         }
     }
     
